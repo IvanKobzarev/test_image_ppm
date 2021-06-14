5819d0286c0919c66b5774ca52aff7eac3a245f9
Ivan Kobzarev <ivankobzarev@fb.com>
2021-06-13 18:00:20 -0700

    Step 2 Complete

--- test.sh
+++ test.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 echo "\n\n\nCheck OK inputs"
-for F in $(find test_input -name 'input_ok_[0-9].ppm')
+for F in $(find test -name 'input_ok_[0-9].ppm')
 do
   echo "\ninput:$F"
   ./build/pipeline -i $F
@@ -11,10 +11,10 @@ do
     echo "TEST FAIL"
   fi
 done
-rm test_input/input_ok_*_out.ppm
+rm test/input_ok_*_out.ppm
 
 echo "\n\n\nCheck FAIL inputs:"
-for F in $(find test_input -name input_fail_[0-9].ppm)
+for F in $(find test -name input_fail_[0-9].ppm)
 do
   echo "\ninput:$F"
   ./build/pipeline -i $F
@@ -29,21 +29,21 @@ done
 echo "\n\n\nCheck output conversion -16"
 
 
-echo "Test: ./test_input/input_ok_0.ppm 8->16 == ./test_input/input_ok_2.ppm"
-./build/pipeline -i ./test_input/input_ok_0.ppm -o ./build/output.ppm -16
-if cmp -s "./test_input/input_ok_2.ppm" "./build/output.ppm"; then
+echo "Test: ./test/input_ok_0.ppm 8->16 == ./test/input_ok_2.ppm"
+./build/pipeline -i ./test/input_ok_0.ppm -o ./build/output.ppm -16
+if cmp -s "./test/input_ok_2.ppm" "./build/output.ppm"; then
   echo "PASS"
 else
   echo "FAIL"
-  diff ./test_input/input_ok_2.ppm ./build/output.ppm
+  diff ./test/input_ok_2.ppm ./build/output.ppm
 fi
 
 
-echo "Test: ./test_input/input_ok_0.ppm 16->8 == ./test_input/input_ok_0.ppm"
-./build/pipeline -i ./test_input/input_ok_2.ppm -o ./build/output.ppm
-if cmp -s "./test_input/input_ok_0.ppm" "./build/output.ppm"; then
+echo "Test: ./test/input_ok_0.ppm 16->8 == ./test/input_ok_0.ppm"
+./build/pipeline -i ./test/input_ok_2.ppm -o ./build/output.ppm
+if cmp -s "./test/input_ok_0.ppm" "./build/output.ppm"; then
   echo "PASS"
 else
   echo "FAIL"
-  diff ./test_input/input_ok_0.ppm ./build/output.ppm
+  diff ./test/input_ok_0.ppm ./build/output.ppm
 fi
